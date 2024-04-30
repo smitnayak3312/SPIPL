437724c8a55f35e504a56c4331aa763bec93a13c
patelsuchi <suchi4287@gmail.com>
2024-04-30 17:12:38 +0530

    5:12

--- src/components/Footer/Terms.jsx
+++ src/components/Footer/Terms.jsx
@@ -1,99 +1,283 @@
-import React from 'react'
+import React from "react";
 import "./terms.css";
 const Terms = () => {
-    return (
-        <div className='header'>
-        <h1>Terms And Condition</h1>
-        <div className='tp_box'>
-            <h3>DEFINITION</h3>
-            <div className='text-box'>
-                <p>Terms of service are the legal agreements between a service provider and a client who wants to use that service. </p><br/>
-                <p>By using our services, these terms will automatically apply to you –you should make sure therefore that you read them carefully before using the services.</p><br/>
-                <p>Client agree to be bound by the terms and conditions below. These terms and conditions are subject to change without notice, from time to time in our sole discretion. We will notify you of amendments to these terms and conditions by posting them to this website.</p>
-            </div>
-
-            <h3>INITIAL WORK</h3>
-            <div className='text-box'>
-            <p>Spike point infotech PVT LTD  will commence the project after getting the required materials from the Client. The home page/ mock-up/design/module would be shown within 10 (may vary) working days. The number of days to complete the home page/application design may vary according to the requirement of the Client. The Client needs to give a definite idea of the design/theme/colour and all aspects of the application/website at the time of preliminary discussion and before commencing the project.The Client can provide samples or older websites/applications as reference.The Spike point infotech PVT LTD team will be creating website pages based on these ideas.</p>
-            </div>
-
-            <h3>CONTENT CLAUSES</h3>
-            <div className='text-box'>
-            <p>Project will commence within 24 hours after the 50% advance payment is made by the Client.The content for the concerned website/application should be provided within 10 days from the time of starting the project. If the content is not provided within the 10 days, there we provide additional time of 2 days for provide the content if customer not provide the content in that time period after that we not accept any content Or anything</p>
-            </div>
-
-            <h3>REDESIGN CLAUSES</h3>
-            <div className='text-box'>
-            <p>If changes are suggested by the Client after the completion and approval of the entire website/application , it will be considered as redesign and are chargeable. Detection of errors in contents or designs by the Client, if any, will be rectified immediately. After completion of the project, The Spike point infotech PVT LTD will upload the whole work in the whatsapp/server and the link will be sent to the Client for testing. The whole work will be uploaded in transferred/made live by The Spike point infotech PVT LTD , after the final approval and settlement of balance amount by the Client.
-            </p>
-            </div>
-
-            <h3>FREE MAINTENANCE</h3>
-            <div className='text-box'>
-            <p>The Spike point infotech PVT LTD provides one year free maintenance towards all kinds of errors occurred in run time of the application /website.</p><br/>
-            <p>Free maintenance does not include content updation.</p><br/>
-            <p>CUSTOMER AND DEVELOPER ISSUES RELATE TO FEATURES AND FUNCTIONALITIES.</p><br/>
-            <p>We are Provides All the Services Who Are The Defines In Your Selected Plan, and All the services and features That You Want And all That who Are defined in SRS(Software Requirement Specification).
-            </p><br/>
-            <p>If Customer Haves More Than Which are Not Define in Plan Then They Don’t Have it because We are Provides Only Those Features Who Is defines In Your Selected Plans.
-            </p>
-            </div>
-
-
-            <h3>COPYRIGHT CLAUSES</h3>
-            <div className='text-box'>
-            <p>Client’re not allowed to copy, or modify the website / application / project, any part of the websites/applications, or our trademarks by any other service provider without us.
-            </p><br/>
-            <p>Client’re not allowed to attempt to extract the source code of the websites/applications, and client also shouldn’t try to translate the app into other languages, or make derivative versions. The app itself, and all the trade marks, copyright, database rights and other intellectual property rights related to it, still belong to Spike point infotech PVT LTD.
-            </p>
-            </div>
-
-            <h3>PAYMENT CLAUSES</h3>
-            <div className='text-box'>
-            <p>Project will commence within 24 hours after the 50% advance payment is made by the Client.
-            </p><br/>
-            <p>Client agrees to make timely payments in accordance with the marked due date on each invoice provided by the Agency, and to include any late fees incurred in each payment.
-            </p><br/>
-            <p>Admin panel ID / password will not be provided until the customer makes full payment.
-            </p><br/>
-            <p>Until the client’s full payment is received, the client’s work will not be received.
-            </p><br/>
-            <p>If the customer signs the agreement, he cannot ask for a refund payment.
-            </p>
-            </div>
-
-            <h3>TERMINATION CLAUSES</h3>
-            <div className='text-box'>
-            <p>Clients may terminate this application/website development services agreement at any time by providing written notice via email or certified mail to Spike point infotech PVT LTD.</p><br/>
-            <p>Spike point infotech PVT LTD  may cancel this agreement in the same manner if necessary.
-            </p><br/>
-            <p>In the event that this application/website development services agreement is cancelled by either party, Spike point infotech PVT LTD  shall issue a final invoice for any unbilled time or materials.
-            </p><br/>
-            <p>Client agrees to pay the final invoice according to the terms of this website development services agreement.
-            </p>
-            </div>
-
-            <h3>CONFLICT RESOLUTION</h3>
-            <div className='text-box'>
-            <p>This application/website development services agreement shall be governed by the prevailing laws of India.</p><br/>
-            <p>Should any conflicts arise related to this agreement, the Parties agree to seek a suitable resolution through a neutral arbitrator, whose ruling shall be considered final and binding on both parties.</p>
-            </div>
-
-            <h3>PROJECT CANCELLATION CONDITIONS</h3>
-            <div className='text-box'>
-            <p>If the client signs the agreement, the order cannot be cancelled.</p><br/>
-            <p>Client cannot ask for a refund payment</p>
-            </div>
-
-            <h3>Contact US</h3>
-            <div className='text-box'>
-            <p>Website : www.starpaneldeveloper.com/<br />
-                Email Id : admin@starpaneldeveloper.com
-            </p>
-            </div>
+  return (
+    <div className="header">
+      <h1>Terms And Condition</h1>
+      <div className="tp_box">
+        <h3>DEFINITION</h3>
+        <div className="text-box">
+          <ul>
+            <li>
+              <p>
+                Terms of service are the legal agreements between a service
+                provider and a client who wants to use that service.
+              </p>
+            </li>
+            <li>
+              <p>
+                By using our services, these terms will automatically apply to
+                you –you should make sure therefore that you read them carefully
+                before using the services.
+              </p>
+            </li>
+            <li>
+              <p>
+                Client agree to be bound by the terms and conditions below.
+                These terms and conditions are subject to change without notice,
+                from time to time in our sole discretion. We will notify you of
+                amendments to these terms and conditions by posting them to this
+                website.
+              </p>
+            </li>
+          </ul>
         </div>
-        </div>
-    )
-}
 
-export default Terms
+        <h3>INITIAL WORK</h3>
+        <div className="text-box">
+          <ul>
+            <li>
+              <p>
+                Spike point infotech PVT LTD will commence the project after
+                getting the required materials from the Client. The home page/
+                mock-up/design/module would be shown within 10 (may vary)
+                working days. The number of days to complete the home
+                page/application design may vary according to the requirement of
+                the Client. The Client needs to give a definite idea of the
+                design/theme/colour and all aspects of the application/website
+                at the time of preliminary discussion and before commencing the
+                project.The Client can provide samples or older
+                websites/applications as reference.The Spike point infotech PVT
+                LTD team will be creating website pages based on these ideas.
+              </p>
+            </li>
+          </ul>
+        </div>
+
+        <h3>CONTENT CLAUSES</h3>
+        <div className="text-box">
+          <ul>
+            <li>
+              <p>
+                Project will commence within 24 hours after the 50% advance
+                payment is made by the Client.The content for the concerned
+                website/application should be provided within 10 days from the
+                time of starting the project. If the content is not provided
+                within the 10 days, there we provide additional time of 2 days
+                for provide the content if customer not provide the content in
+                that time period after that we not accept any content Or
+                anything
+              </p>
+            </li>
+          </ul>
+        </div>
+
+        <h3>REDESIGN CLAUSES</h3>
+        <div className="text-box">
+          <ul>
+            <li>
+              <p>
+                If changes are suggested by the Client after the completion and
+                approval of the entire website/application , it will be
+                considered as redesign and are chargeable. Detection of errors
+                in contents or designs by the Client, if any, will be rectified
+                immediately. After completion of the project, The Spike point
+                infotech PVT LTD will upload the whole work in the
+                whatsapp/server and the link will be sent to the Client for
+                testing. The whole work will be uploaded in transferred/made
+                live by The Spike point infotech PVT LTD , after the final
+                approval and settlement of balance amount by the Client.
+              </p>
+            </li>
+          </ul>
+        </div>
+
+        <h3>FREE MAINTENANCE</h3>
+        <div className="text-box">
+          <ul>
+            <li>
+              <p>
+                The Spike point infotech PVT LTD provides one year free
+                maintenance towards all kinds of errors occurred in run time of
+                the application /website.
+              </p>
+            </li>
+         
+
+          <li>
+            <p>Free maintenance does not include content updation.</p>
+          </li>
+
+          <li>
+            <p>
+              CUSTOMER AND DEVELOPER ISSUES RELATE TO FEATURES AND
+              FUNCTIONALITIES.
+            </p>
+          </li>
+          <li>
+            <p>
+              We are Provides All the Services Who Are The Defines In Your
+              Selected Plan, and All the services and features That You Want And
+              all That who Are defined in SRS(Software Requirement
+              Specification).
+            </p>
+          </li>
+          <li>
+           
+            <p>
+              If Customer Haves More Than Which are Not Define in Plan Then They
+              Don’t Have it because We are Provides Only Those Features Who Is
+              defines In Your Selected Plans.
+            </p>
+          </li>
+          </ul>
+        </div>
+
+        <h3>COPYRIGHT CLAUSES</h3>
+        <div className="text-box">
+          <ul>
+            <li>
+              <p>
+                Client’re not allowed to copy, or modify the website /
+                application / project, any part of the websites/applications, or
+                our trademarks by any other service provider without us.
+              </p>
+            </li>
+            <li>
+              <p>
+                Client’re not allowed to attempt to extract the source code of
+                the websites/applications, and client also shouldn’t try to
+                translate the app into other languages, or make derivative
+                versions. The app itself, and all the trade marks, copyright,
+                database rights and other intellectual property rights related
+                to it, still belong to Spike point infotech PVT LTD.
+              </p>
+            </li>
+          </ul>
+        </div>
+
+        <h3>PAYMENT CLAUSES</h3>
+        <div className="text-box">
+          <ul>
+            <li>
+              <p>
+                Project will commence within 24 hours after the 50% advance
+                payment is made by the Client.
+              </p>
+            </li>
+            <li>
+              <p>
+                Client agrees to make timely payments in accordance with the
+                marked due date on each invoice provided by the Agency, and to
+                include any late fees incurred in each payment.
+              </p>
+            </li>
+            <li>
+              <p>
+                Admin panel ID / password will not be provided until the
+                customer makes full payment.
+              </p>
+            </li>
+            <li>
+              <p>
+                Until the client’s full payment is received, the client’s work
+                will not be received.
+              </p>
+            </li>
+            <li>
+              <p>
+                If the customer signs the agreement, he cannot ask for a refund
+                payment.
+              </p>
+            </li>
+          </ul>
+        </div>
+
+        <h3>TERMINATION CLAUSES</h3>
+        <div className="text-box">
+          <ul>
+            <li>
+              <p>
+                Clients may terminate this application/website development
+                services agreement at any time by providing written notice via
+                email or certified mail to Spike point infotech PVT LTD.
+              </p>
+            </li>
+            <li>
+              <p>
+                Spike point infotech PVT LTD may cancel this agreement in the
+                same manner if necessary.
+              </p>
+            </li>
+            <li>
+              <p>
+                In the event that this application/website development services
+                agreement is cancelled by either party, Spike point infotech PVT
+                LTD shall issue a final invoice for any unbilled time or
+                materials.
+              </p>
+            </li>
+            <li>
+              {" "}
+              <p>
+                Client agrees to pay the final invoice according to the terms of
+                this website development services agreement.
+              </p>
+            </li>
+          </ul>
+        </div>
+
+        <h3>CONFLICT RESOLUTION</h3>
+        <div className="text-box">
+          <ul>
+            <li>
+              <p>
+                This application/website development services agreement shall be
+                governed by the prevailing laws of India.
+              </p>
+            </li>
+            <li>
+              <p>
+                Should any conflicts arise related to this agreement, the
+                Parties agree to seek a suitable resolution through a neutral
+                arbitrator, whose ruling shall be considered final and binding
+                on both parties.
+              </p>
+            </li>
+          </ul>
+        </div>
+
+        <h3>PROJECT CANCELLATION CONDITIONS</h3>
+        <div className="text-box">
+          <ul>
+            <li>
+              <p>
+                If the client signs the agreement, the order cannot be
+                cancelled.
+              </p>
+            </li>
+            <li>
+              <p>
+                If the client signs the agreement, the order cannot be
+                cancelled.
+              </p>
+            </li>
+            <li>
+              <p>Client cannot ask for a refund payment</p>
+            </li>
+          </ul>
+        </div>
+
+        <h3>Contact US</h3>
+        <div className="text-box">
+          <p>
+            <li>Website : www.starpaneldeveloper.com/</li>
+           <li> Email Id : admin@starpaneldeveloper.com</li>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Terms;
